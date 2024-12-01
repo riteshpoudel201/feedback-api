3c5aa5e875f52f44c1aac479b3430e227f37c449
Ritesh Poudel <39463468+riteshpoudel201@users.noreply.github.com>
2024-12-01 20:27:31 +0545

    feedback api first commit

---
+++ index.js
@@ -0,0 +1,45 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import axios from "axios";
+const PORT = 8000;
+const app= express();
+app.use(express.json());
+app.use(cors())
+dotenv.config();
+app.post("/feedback", async (req,res)=>{
+    const { from, to, name, email, messageBody} = req.body;
+
+    const Email = {
+        from,
+        to,
+        subject:`From ${email}`,
+        html: messageBody,
+      }
+      try{
+      const response = await axios.post('https://api.resend.com/emails', Email ,{
+        headers:{
+         "Authorization":`Bearer ${process.env.RESEND_API_KEY}`
+        }
+       })
+       if(response.data){
+        res.status(201).json({
+            success:true,
+            data: response.data
+        })
+       }
+      }catch(error){
+        console.log(error);
+        res.json({
+            success:false,
+            data: error
+        })
+       }
+})
+app.listen(PORT, (error)=>{
+    if(error){
+        console.log(error)
+    }else{
+        console.log("Server is up and running at: ", PORT)
+    }
+})
